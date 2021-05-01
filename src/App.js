f6d3e8d8d1df99f3fc5b140d046e9d1bbfeb7983
Rishab Rao <rishabpaws@gmail.com>
2021-05-01 15:54:44 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Login from './components/login/Login'
 import Home from './Home'
-
+import Finder from './components/finder/Finder'
+import nav from './components/nav/Nav'
+import Match from './components/match/Match'
+import Profile from './components/profile/Profile'
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -9,12 +13,17 @@ import './App.css';
 
 function App() {
   return (
-      <Router>   
+    <Router>   
       <div className="home-default">  
 
-      <Home/>
-          
-]    </div>
+         
+          <Route path="/login" Component={Login}/>
+          <Route exact path="/" Component={nav, Home}/>
+          <Route path="/matches" component={nav, Match} />
+      <Route path="/discover" component={nav, Finder} />
+      <Route path="/profile" component={nav, Profile} />
+          {/* <Login/> */}
+    </div>
     </Router> 
   
   );
