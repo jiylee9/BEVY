0c82a4a4b82ddbad0462a9c9638217ed5abf8cb9
Explicious <46688743+Explicious@users.noreply.github.com>
2021-05-01 14:42:42 -0500

    Added edit button to profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -20,15 +20,17 @@ class Profile extends React.Component {
       <div className="container">
         <img className="profile-image" src="https://i.imgur.com/hzRXXd1.jpg" alt="kati wen"></img>
         
-        <div className="vertical-center">
-          <div className="profile-bio">
-            <h2 className="profile-name">Katie Nguyen</h2>
-
-            <h3 className="profile-cm">University of Illinois at Urbana Champaign</h3>
-
-            <h3 className="profile-cm">Chemical Engineering</h3>
-          </div>
+        <div className="edit-btn">
+          <img src="https://i.imgur.com/x7Pez6T.png"></img>
         </div>
+        <div className="profile-bio">
+          <h2 className="profile-name">Katie Nguyen 18</h2>
+
+          <h3 className="profile-cm">University of Illinois at Urbana Champaign</h3>
+
+          <h3 className="profile-cm">Chemical Engineering</h3>
+        </div>
+        
 
       </div>
     );
