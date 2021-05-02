78974fef7cd3c9b128311a733418972b60630402
JiYoon Lee <57721847+jiylee9@users.noreply.github.com>
2021-05-01 19:10:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Finder from './components/finder/Finder'
 import Match from './components/match/Match'
 import Profile from './components/profile/Profile'
 import Home from './Home'
+import Login from './components/login/Login'
+
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -15,7 +17,11 @@ function App() {
     <Router>   
       <div className="home-default">  
 
-        <Home/>
+      <Nav />
+      <Route path='/matches' component={Match} />
+      <Route path='/discover' component={Finder} />      
+      <Route path='/profile' component={Profile}/>
+      <Route exact path='/' component={Login}/>
 
 
       </div>
